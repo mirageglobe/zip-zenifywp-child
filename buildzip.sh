d50eed28633a1519cfdf642ba6c27013658e37df
Jimmy MG Lim <mirageglobe@users.noreply.github.com>
2018-11-01 18:32:16 +0000

    Update buildzip.sh

--- buildzip.sh
+++ buildzip.sh
@@ -2,4 +2,4 @@
 
 echo "[+] Compressing for Wordpress"
 #zip -r -X -9 -x *.git* zenifywordpress-child.zip .
-git archive --format=zip HEAD -o zenifywordpress-child.zip
+git archive --format=zip HEAD -o zenifywp-child.zip
